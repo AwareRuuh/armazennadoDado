535f7509f5100e4f31383344865fa46e435d6ee6
Ruth <88747761+AwareRuuh@users.noreply.github.com>
2022-10-08 17:48:46 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Main {
+  //MAIN SEMPRE EM MAISCULO QUANDO ESTIVER EM MAISCULO!
+  public static void main(String[] args) {
+    Scanner sc1 = new Scanner(System.in);
+    System.out.println("Digite um número:");
+    int a = sc1.nextInt();
+  }
+}
